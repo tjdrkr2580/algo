200475124492744dbe1167b902403226446ee4f9
김태현 <97386129+tjdrkr2580@users.noreply.github.com>
2023-06-21 00:54:37 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.06 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,33 +1,29 @@
 function solution(number, limit, power) {
-    let totalWeight = 0; // 필요한 철의 총 무게를 저장할 변수
+    let answer = 0; // 필요한 철의 총 무게를 저장할 변수
 
-    // 각 기사에 대해
     for (let i = 1; i <= number; i++) {
         let factors = getFactors(i); // 기사의 번호의 약수를 구함
-        let attackPower = factors.length; // 약수의 개수가 공격력이 됨
+        let powerPoint = factors.length; // 약수의 개수가 공격력이 됨
 
-        // 공격력이 제한수치보다 크면 power를 사용
-        if (attackPower > limit) {
-            attackPower = power;
+        if (powerPoint > limit) {
+            powerPoint = power;
         }
 
-        totalWeight += attackPower; // 총 무게에 공격력을 더함 (공격력 1당 철 1kg 필요)
+        answer += powerPoint; // 총 무게에 공격력을 더함 (공격력 1당 철 1kg 필요)
     }
 
-    return totalWeight; // 필요한 철의 총 무게를 반환
+    return answer;
 }
 
 function getFactors(num) {
-    let factors = [];
-
+    let factorArr = [];
     for (let i = 1; i <= Math.sqrt(num); i++) {
         if (num % i === 0) {
-            factors.push(i);
+            factorArr.push(i);
             if (num / i !== i) {
-                factors.push(num / i);
+                factorArr.push(num / i);
             }
         }
     }
-
-    return factors;
+    return factorArr;
 }
